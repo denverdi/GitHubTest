973aeea5569cee3d85582a9932f93915fb01b1be
Mofor <didier.diffo@interfacesa.com>
2015-10-30 12:43:06 +0100

    Indentation

--- GitHubTest/src/Test.java
+++ GitHubTest/src/Test.java
@@ -6,20 +6,24 @@
  * @author d.diffo
  *
  */
-public class Test {
+public class Test
+{
 
 	/**
 	 * 
 	 */
-	//un comentaire de plus
-	public Test() {
+	// un comentaire de plus
+	public Test()
+	{
+
 		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
